0c7d95c9e6cc5112110d5fefff474f012ac218b9
mezankhaja <38165296+mezankhaja@users.noreply.github.com>
2019-02-12 21:40:31 -0500

    Fixed hardcoded file path

--- Python/Day-1.py
+++ Python/Day-1.py
@@ -1,8 +1,11 @@
 import os
 import re
 
-input_file = os.path.normpath("C:/Users/mezan/Documents/Deep Learning Nanodegree/Advent-of-Code-2018/Inputs/Day-1-Input")
+fileDir = os.path.dirname(os.path.abspath('__file__'))
+input_file = os.path.join(fileDir, '.\Inputs\Day-1-Input')
 f = open(input_file)
+
 freqs = f.readlines()
 freqs = [int(re.sub('\s*','',f)) for f in freqs]
-print(sum(freqs))
+
+print(sum(freqs))   
