1d06a0adc098809935cac59a02a4c4ae42e1a192
mezankhaja <38165296+mezankhaja@users.noreply.github.com>
2019-02-12 21:18:08 -0500

    Correct output

--- Python/Day-1.py
+++ Python/Day-1.py
@@ -1,1 +1,8 @@
-#hello
+import os
+import re
+
+input_file = os.path.normpath("C:/Users/mezan/Documents/Deep Learning Nanodegree/Advent-of-Code-2018/Inputs/Day-1-Input")
+f = open(input_file)
+freqs = f.readlines()
+freqs = [int(re.sub('\s*','',f)) for f in freqs]
+print(sum(freqs))
